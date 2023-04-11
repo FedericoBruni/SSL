dabfa7543f3bcf1298f4491408a27cc56b0de29c
FedericoBruni <fbruni@frba.utn.edu.ar>
2023-04-10 22:42:47 -0300

    Add files via upload

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    FILE *archivo;
-    archivo = fopen("output.txt", "w");
-    if (archivo == NULL){
-        printf("Error al abrir el archivo.\n");
-        return 1;
-    }
-    fprintf(archivo, "Hello, World!\n");
+    printf("Hello, World!\n");
     return 0;
 }
